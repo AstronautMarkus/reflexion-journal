e5ebf0a8b655da6bceb720b20bab540effd67b9b
Marcos Reyes <astronautmarkus@gmail.com>
2025-10-23 03:08:46 -0300

    feat: display day count since user defined day zero on dashboard

--- reflexion-journal/app/routes/journal/dashboard.py
+++ reflexion-journal/app/routes/journal/dashboard.py
@@ -2,7 +2,8 @@ from . import journal
 from flask import Flask, render_template
 from flask_login import current_user
 from app.middleware.check_user_auth import login_required_middleware
-from app.models.models import User, UserDayZero
+from app.models.models import UserDayZero
+from datetime import date
 
 @journal.route('/journal/dashboard')
 @login_required_middleware
@@ -11,9 +12,13 @@ def dashboard():
 
     user_day_zero = UserDayZero.query.filter_by(user_id=user.id).first()
     show_day_zero_alert = not bool(user_day_zero)
+    
+    if user_day_zero:
+        day_count = (date.today() - user_day_zero.date).days + 1
 
     return render_template(
         'journal/dashboard.html',
         user=user,
-        show_day_zero_alert=show_day_zero_alert
+        show_day_zero_alert=show_day_zero_alert,
+        day_count=day_count if user_day_zero else None
     )
