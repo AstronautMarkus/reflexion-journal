2a35375eb8d78349fe912babb3b154ecaeb6dc75
Marcos Reyes <astronautmarkus@gmail.com>
2025-10-23 02:26:34 -0300

    feat: Update dashboard route and template to include user greeting

--- reflexion-journal/app/routes/journal/dashboard.py
+++ reflexion-journal/app/routes/journal/dashboard.py
@@ -1,8 +1,10 @@
 from . import journal
 from flask import Flask, render_template
+from flask_login import current_user
 from app.middleware.check_user_auth import login_required_middleware
 
-@journal.route('/dashboard')
+@journal.route('/journal/dashboard')
 @login_required_middleware
 def dashboard():
-    return render_template('journal/dashboard.html')
+    user = current_user
+    return render_template('journal/dashboard.html', user=user)
