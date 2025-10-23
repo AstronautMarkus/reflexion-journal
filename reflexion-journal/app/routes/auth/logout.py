79a861a8666b0c48da06017e433d82e29894b087
Marcos Reyes <astronautmarkus@gmail.com>
2025-10-23 02:55:20 -0300

    fix: implemented logout from flask login in logout endpoint.

--- reflexion-journal/app/routes/auth/logout.py
+++ reflexion-journal/app/routes/auth/logout.py
@@ -1,8 +1,10 @@
 from . import auth
 from flask import session, redirect, url_for, flash
+from flask_login import logout_user
 
 @auth.route('/logout')
 def logout():
+    logout_user()
     session.pop('user_id', None)
     flash('Sesión cerrada correctamente.', 'success')
     return redirect(url_for('auth.login'))
