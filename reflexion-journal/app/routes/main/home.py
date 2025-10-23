ba6c5192dbf5fd45fe6e6f820233a62545d4fc01
Marcos Reyes <astronautmarkus@gmail.com>
2025-10-23 02:22:05 -0300

    feat: Redirect authenticated users to journal home from the main page

--- reflexion-journal/app/routes/main/home.py
+++ reflexion-journal/app/routes/main/home.py
@@ -1,6 +1,10 @@
 from . import main
-from flask import Flask, render_template
+from flask import render_template, redirect, url_for
+from flask_login import current_user
 
 @main.route('/')
 def home():
-    return render_template('home.html')
+    if current_user.is_authenticated:
+        return redirect(url_for('journal.home'))
+    else:
+        return render_template('home.html')
